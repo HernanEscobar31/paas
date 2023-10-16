a83c784a7122a5f8acfb7ae9b4066dd3fbafaaee
HernanEscobar31 <111523308+HernanEscobar31@users.noreply.github.com>
2023-10-16 16:35:17 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,8 @@
 <?php
-    
-    try{
-         $conexion = new PDO('mysql:host=http://104.248.118.220/;dbname=prueba', 'root', 'root1234');
-    }catch(PDOException $prueba_error){
-        echo "Error: " . $prueba_error->getMessage();
-    }
-
+    $host:"app-2bbb20e4-f2f2-4768-b331-cc0fbd916b61-do-user-14722467-0.b.db.ondigitalocean.com";
+    $user:"db";
+    $psw:"AVNS_4mHvhKIF8I9AyZS5gOv";
+    $db:"db";
+    $conexion=mysqli_connect($host,$user,$psw,$db);
 
 ?>
