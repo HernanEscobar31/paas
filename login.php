119b636555b05e48849f8ba1049c5a6717bdf3e6
HernanEscobar31 <111523308+HernanEscobar31@users.noreply.github.com>
2023-10-15 20:24:44 -0600

    Update login.php

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
         $clave = hash('sha512', $clave);
         
         try{
-            $conexion = new PDO('mysql:host=104.248.118.220;dbname=prueba', 'root', 'root1234');
+            $conexion = new PDO('mysql:host=http://104.248.118.220/;dbname=prueba', 'root', 'root1234');
             }catch(PDOException $prueba_error){
                 echo "Error: " . $prueba_error->getMessage();
             }
