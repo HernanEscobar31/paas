090dfb9a02f4dc06d2ab6a1bf7e22b5672211649
HernanEscobar31 <111523308+HernanEscobar31@users.noreply.github.com>
2023-10-15 18:58:50 -0600

    Update login.php

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
         $clave = hash('sha512', $clave);
         
         try{
-            $conexion = new PDO('mysql:host=localhost;dbname=db', 'db', 'AVNS_gJakkJxzypCA5wlJ7tb');
+            $conexion = new PDO('mysql:host=app-bb69b715-1dd4-4374-b5b5-dc9b8529f414-do-user-14722467-0.b.db.ondigitalocean.com;dbname=db', 'db', 'AVNS_gJakkJxzypCA5wlJ7tb');
             }catch(PDOException $prueba_error){
                 echo "Error: " . $prueba_error->getMessage();
             }
