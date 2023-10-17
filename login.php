dc1d77dd5553f0311f22a4b9e295bf478ee073a4
HernanEscobar31 <111523308+HernanEscobar31@users.noreply.github.com>
2023-10-17 15:08:36 -0600

    Update login.php

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
         $clave = $_POST['clave'];
         $clave = hash('sha512', $clave);
         
-        $conexion = pg_connect("host=app-2bbb20e4-f2f2-4768-b331-cc0fbd916b61-do-user-14722467-0.b.db.ondigitalocean.com dbname=db user=db password=AVNS_4mHvhKIF8I9AyZS5gOv");
+        $conexion = pg_connect("host=app-2bbb20e4-f2f2-4768-b331-cc0fbd916b61-do-user-14722467-0.b.db.ondigitalocean.com dbname=db user=db password=AVNS_gN537HosKlBDReoK68u");
         
         $statement = $conexion->prepare('
         SELECT * FROM login WHERE usuario = :usuario AND clave = :clave'
