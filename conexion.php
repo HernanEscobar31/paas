bb658f899ce08c0b1fe5bacd1388e1c1738fb6a5
HernanEscobar31 <111523308+HernanEscobar31@users.noreply.github.com>
2023-10-17 14:29:53 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,4 @@
 <?php
-    $host:"app-2bbb20e4-f2f2-4768-b331-cc0fbd916b61-do-user-14722467-0.b.db.ondigitalocean.com";
-    $user:"db";
-    $psw:"AVNS_4mHvhKIF8I9AyZS5gOv";
-    $db:"db";
-    $conexion=mysqli_connect($host,$user,$psw,$db);
+ $conexion = pg_connect("host=app-2bbb20e4-f2f2-4768-b331-cc0fbd916b61-do-user-14722467-0.b.db.ondigitalocean.com dbname=db user=db password=AVNS_4mHvhKIF8I9AyZS5gOv");
 
 ?>
