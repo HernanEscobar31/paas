4e0dd7ea250f0f59ca8955058c40970d5c1c0c90
HernanEscobar31 <111523308+HernanEscobar31@users.noreply.github.com>
2023-10-17 14:25:22 -0600

    Update register.php

--- register.php
+++ register.php
@@ -22,16 +22,45 @@
             
             $error .= '<i>Favor de rellenar todos los campos</i>';
         }else{
-            function conectar_PostgreSQL( $usuario, $pass, $host, $bd, $port ){
-            $conexion = pg_connect( "user=".$usuario." ".
-                                "password=".$pass." ".
-                                "host=".$host." ".
-                                "dbname=".$bd." ".
-                                "port=".$port
-                              ) or die( "Error al conectar: ".pg_last_error() );
+            <?php session_start();
 
-        return $conexion;
+    if(isset($_SESSION['usuario'])) {
+        header('location: index.php');
+    }
+
+    $error = '';
+    
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+        $usuario = $_POST['usuario'];
+        $clave = $_POST['clave'];
+        $clave = hash('sha512', $clave);
+        
+        $conexion = pg_connect("host=app-2bbb20e4-f2f2-4768-b331-cc0fbd916b61-do-user-14722467-0.b.db.ondigitalocean.com dbname=db user=db password=AVNS_4mHvhKIF8I9AyZS5gOv");
+        
+        $statement = $conexion->prepare('
+        SELECT * FROM login WHERE usuario = :usuario AND clave = :clave'
+        );
+        
+        $statement->execute(array(
+            ':usuario' => $usuario,
+            ':clave' => $clave
+        ));
+            
+        $resultado = $statement->fetch();
+        
+        if ($resultado !== false){
+            $_SESSION['usuario'] = $usuario;
+            header('location: principal.php');
+        }else{
+            $error .= '<i>Este usuario no existe</i>';
         }
+    }
+    
+require 'frontend/login-vista.php';
+
+
+?>
             
             $statement = $conexion->prepare('SELECT * FROM login WHERE usuario = :usuario LIMIT 1');
             $statement->execute(array(':usuario' => $usuario));
