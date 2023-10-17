31b285e36fe222561dda2fc8d23756604e6c6896
HernanEscobar31 <111523308+HernanEscobar31@users.noreply.github.com>
2023-10-17 14:29:02 -0600

    Update register.php

--- register.php
+++ register.php
@@ -22,45 +22,7 @@
             
             $error .= '<i>Favor de rellenar todos los campos</i>';
         }else{
-            <?php session_start();
-
-    if(isset($_SESSION['usuario'])) {
-        header('location: index.php');
-    }
-
-    $error = '';
-    
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        
-        $usuario = $_POST['usuario'];
-        $clave = $_POST['clave'];
-        $clave = hash('sha512', $clave);
-        
-        $conexion = pg_connect("host=app-2bbb20e4-f2f2-4768-b331-cc0fbd916b61-do-user-14722467-0.b.db.ondigitalocean.com dbname=db user=db password=AVNS_4mHvhKIF8I9AyZS5gOv");
-        
-        $statement = $conexion->prepare('
-        SELECT * FROM login WHERE usuario = :usuario AND clave = :clave'
-        );
-        
-        $statement->execute(array(
-            ':usuario' => $usuario,
-            ':clave' => $clave
-        ));
-            
-        $resultado = $statement->fetch();
-        
-        if ($resultado !== false){
-            $_SESSION['usuario'] = $usuario;
-            header('location: principal.php');
-        }else{
-            $error .= '<i>Este usuario no existe</i>';
-        }
-    }
-    
-require 'frontend/login-vista.php';
-
-
-?>
+            $conexion = pg_connect("host=app-2bbb20e4-f2f2-4768-b331-cc0fbd916b61-do-user-14722467-0.b.db.ondigitalocean.com dbname=db user=db password=AVNS_4mHvhKIF8I9AyZS5gOv");
             
             $statement = $conexion->prepare('SELECT * FROM login WHERE usuario = :usuario LIMIT 1');
             $statement->execute(array(':usuario' => $usuario));
